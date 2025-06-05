dbdcaf6c17ce1d52bbf997b7ad81ef36ffa48442
ayush-ac <ayush9044182486@gmail.com>
2025-06-05 16:49:22 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './component/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
      
-      <h1 className='text-green-800'>Vite + React</h1>
+     <Navbar/>
      
     </>
   )
